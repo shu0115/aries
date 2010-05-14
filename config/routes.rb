988a321365b5d207c4be6af5963228eb2f83f7bb
Shun Matsumoto <ShunMatsumoto@shu-iMac.local>
2010-05-14 22:21:22 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,8 @@ ActionController::Routing::Routes.draw do |map|
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect '', :controller => 'memos', :action => 'list'
+  map.connect '/memos', :controller => 'memos', :action => 'list'
+
   map.connect ':controller', :action => 'index'
 
   map.connect ':controller/:action/:id'

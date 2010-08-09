12e05849d03f828e5d2af241cb99020b2a31f087
shu_0115 <raisondetre0115@gmail.com>
2010-08-09 23:30:43 +0900

    update

--- db/migrate/20100516082014_add.rb
+++ db/migrate/20100516082014_add.rb
@@ -1,6 +1,7 @@
 class Add < ActiveRecord::Migration
   def self.up
-    add_column :memos, :user_id, :string
+#    add_column :memos, :user_id, :string
+    add_column :memos, :user_id, :integer
   end
 
   def self.down

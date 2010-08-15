0d3ba23fddd0ae2535cb07f64106e951ac9255c0
shu_0115 <raisondetre0115@gmail.com>
2010-08-15 18:49:19 +0900

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,3 +15,6 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+# log
+config.logger = Logger.new( config.log_path, 1, 10*1024*1024 )  # 10MB／1ファイル
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,9 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
+# log
+config.logger = Logger.new( config.log_path, 10, 10*1024*1024 )  # 10MB／10ファイル
+
 # See everything in the log (default is :info)
 # config.log_level = :debug
 

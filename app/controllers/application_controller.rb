ff1d5a74c6209409e2cc4c14a19587b31b8c2990
Shun Matsumoto <raisondetre0115@gmail.com>
2011-02-27 20:31:57 +0900

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   $link_name = { :show => "Show", :edit => "Edit", :delete => "Delete", :new => "New", :list => "List" }
 
   # ページング
-  $per_page = 20
+  $per_page = 15
 
   # 基本色
   $base_color = "#70c0ff"
